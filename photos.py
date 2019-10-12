24c0bca11418faa90489927c7c074af7410bf599
danieladmon <54929117+danieladmon@users.noreply.github.com>
2019-10-12 19:33:38 +0300

    Update photos.py

--- photos.py
+++ photos.py
@@ -1,9 +1,9 @@
 import os
-dir="C:\\Users\\DA\\Desktop\\motorcity"
+dir = "C:\\Users\DA\Desktop\motorcity"
 
 for f in os.listdir(dir):
     if os.path.splitext(f)[1] == '.CR2':
-        fname = dir+"\\"+f.split(".")[0]+".JPG"
+        fname = dir + "\\" + f.split(".")[0]+ ".JPG"
         if os.path.isfile(fname) == False:
             print (fname)
-            os.rename(dir+ '\\'+f,dir+'\\del\\'+f)
+            os.rename(dir + '\\' + f, dir + '\\del\\' +f)
