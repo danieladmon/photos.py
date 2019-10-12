ed70e8679850b89c26bcba1920f1ff48e38a4875
danieladmon <54929117+danieladmon@users.noreply.github.com>
2019-10-12 21:39:42 +0300

    Update photos.py

--- photos.py
+++ photos.py
@@ -1,9 +1,30 @@
+'''
+photos.py by Daniel Admon
+
+this script is made to make my life easier.
+I'm a photographer that shout at RAW and JPEG.
+why JPEG and RAW ? because its faster to view and organize photos with JPEG.
+RAW is a large file and its slower then JPEG.
+'''
 import os
-dir = "C:\\Users\DA\Desktop\XXX"
+
+dir = "C:\\Users\DA\Desktop\motorcity"
+deldir = dir + "\\del"
+count = 0
+
+if not os.path.exists(deldir):
+    os.makedirs(deldir)
 
 for f in os.listdir(dir):
-    if os.path.splitext(f)[1] == '.CR2':
+    if os.path.splitext(f)[1] == ".CR2":
         fname = dir + "\\" + f.split(".")[0]+ ".JPG"
         if os.path.isfile(fname) == False:
-            print (fname)
-            os.rename(dir + '\\' + f, dir + '\\del\\' +f)
+            fpath = dir + '\\' + f
+            if os.path.isfile(fpath):
+                print(fname)
+                os.rename(fpath,deldir + "\\" + f)
+                count += 1
+            else:
+                print("file ",fname," already exist")
+
+print("total moved: ",count)
