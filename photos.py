3beb8afc6a16acffc2db9f9c9b879854482eec0a
danieladmon <54929117+danieladmon@users.noreply.github.com>
2019-10-13 00:49:18 +0300

    Update photos.py

--- photos.py
+++ photos.py
@@ -8,16 +8,20 @@ RAW is a large file and its slower then JPEG.
 '''
 import os
 
-dir = "C:\\Users\DA\Desktop\motorcity"
-deldir = dir + "\\del"
-count = 0
+dir = os.getcwd()
+#dir = input("Please type path to the folder:  ")
+deldir = dir + "\\del"; count = 0
+
+if not os.path.exists(dir):
+    print(f"Error: directory \"{dir}\" doesn't exist.")
+    exit()
 
 if not os.path.exists(deldir):
     os.makedirs(deldir)
 
 for f in os.listdir(dir):
-    if os.path.splitext(f)[1] == ".CR2":
-        fname = dir + "\\" + f.split(".")[0]+ ".JPG"
+    if f.endswith(".CR2"):
+        fname = dir + "\\" + f.split(".")[0] + ".JPG"
         if os.path.isfile(fname) == False:
             fpath = dir + '\\' + f
             if os.path.isfile(fpath):
@@ -25,6 +29,6 @@ for f in os.listdir(dir):
                 os.rename(fpath,deldir + "\\" + f)
                 count += 1
             else:
-                print("file ",fname," already exist")
+                print(f"Error: file \"{fname}\" already exist.")
 
-print("total moved: ",count)
+print(f"total moved: {count}")
