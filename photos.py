c3d9ae49a7bb4992d4fe970db72986c67e8dfe93
danieladmon <54929117+danieladmon@users.noreply.github.com>
2019-10-28 13:28:26 +0200

    Update photos.py

--- photos.py
+++ photos.py
@@ -2,7 +2,7 @@
 photos.py by Daniel Admon
 
 this script is made to make my life easier.
-I'm a photographer that shout at RAW and JPEG.
+I'm a photographer that shout in RAW and JPEG.
 why JPEG and RAW ? because its faster to view and organize photos with JPEG.
 RAW is a large file and its slower then JPEG.
 '''
