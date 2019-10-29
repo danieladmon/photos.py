8fa6cdea6e1470937341c54831e4b0f7e2a5202f
danieladmon <54929117+danieladmon@users.noreply.github.com>
2019-10-29 15:22:24 +0200

    Update photos.py

--- photos.py
+++ photos.py
@@ -1,10 +1,13 @@
 '''
 photos.py by Daniel Admon
 
-this script is made to make my life easier.
-I'm a photographer that shout in RAW and JPEG.
-why JPEG and RAW ? because its faster to view and organize photos with JPEG.
-RAW is a large file and its slower then JPEG.
+This script is made to make my life easier.
+I'm a photographer that shoot in RAW and JPEG.
+why JPEG and RAW ? because RAW is a large file and its slower then JPEG
+so its faster to view and organize photos with JPEG,
+and when i'm editing the photos i'm using the RAW files.
+
+The script check if there are RAW file without the JPG file and delete the RAW file.
 '''
 import os
 
